cf66cedc7be7c4f3ddcef09d3093c747787c47fd
mkonzal <72101148+mkonzal@users.noreply.github.com>
2021-08-18 11:17:21 +0200

    Remove Provider annotation

--- src/main/java/org/acme/rest/client/CustomResponseExceptionMapper.java
+++ src/main/java/org/acme/rest/client/CustomResponseExceptionMapper.java
@@ -1,15 +1,11 @@
 package org.acme.rest.client;
 
-import javax.annotation.Priority;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.Provider;
 
 import org.eclipse.microprofile.rest.client.ext.ResponseExceptionMapper;
 import org.jboss.logging.Logger;
 
-@Provider
-@Priority(0)
 public class CustomResponseExceptionMapper implements ResponseExceptionMapper<MyException> {
     @Override
     public boolean handles(final int status, final MultivaluedMap<String, Object> headers) {
